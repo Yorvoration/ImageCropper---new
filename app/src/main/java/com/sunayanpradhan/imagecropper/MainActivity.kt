e3f373c69dde72c1d7c57c2bac923b4588e8c7bb
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-09-29 18:52:39 +0500

    image cropper works

--- app/src/main/java/com/sunayanpradhan/imagecropper/MainActivity.kt
+++ app/src/main/java/com/sunayanpradhan/imagecropper/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sunayanpradhan.imagecropper
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues
 import android.content.Context
@@ -14,11 +15,11 @@ import android.os.Environment
 import android.provider.MediaStore
 import android.view.Gravity
 import android.view.View
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -34,161 +35,99 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
-    private val GALLERY_REQUEST_CODE = 1234
-    private val WRITE_EXTERNAL_STORAGE_CODE=1
-
-    lateinit var activityResultLauncher:ActivityResultLauncher<Intent>
-
-    lateinit var finalUri: Uri
+    private val galleryCode = 1234
+    private val exTrNalStorageCode = 1
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var finalUri: Uri
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
         setContentView(R.layout.activity_main)
-        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
-
-
-
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         checkPermission()
         requestPermission()
 
-
-
         binding.gallery.setOnClickListener {
-
             if (checkPermission()) {
-
                 pickFromGallery()
-            }
-
-            else{
+            } else {
                 Toast.makeText(this, "Allow all permissions", Toast.LENGTH_SHORT).show()
                 requestPermission()
             }
-
         }
 
         binding.camera.setOnClickListener {
-
             if (checkPermission()) {
-
                 pickFromCamera()
-            }
-
-            else{
+            } else {
                 Toast.makeText(this, "Allow all permissions", Toast.LENGTH_SHORT).show()
                 requestPermission()
             }
-
         }
 
         binding.save.setOnClickListener {
-
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-
-                if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
-
-                    val permission = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-
-                    requestPermissions(permission,WRITE_EXTERNAL_STORAGE_CODE)
-
-
-                }
-                else{
-
-                    saveEditedImage()
-
-                }
-
-
+            if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                val permission = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                requestPermissions(permission, exTrNalStorageCode)
+            } else {
+                saveEditedImage()
             }
-
-
         }
 
         binding.cancel.setOnClickListener {
-
             val builder = AlertDialog.Builder(this)
             builder.setMessage("DO YOU WANT TO CANCEL?")
-            builder.setPositiveButton("YES") { dialog, which ->
+            builder.setPositiveButton("YES") { _, _ ->
 
-                binding.image.visibility=View.GONE
-                binding.cancel.visibility=View.GONE
-                binding.save.visibility=View.GONE
-                binding.selectToast.visibility=View.VISIBLE
-                binding.camera.visibility=View.VISIBLE
-                binding.gallery.visibility=View.VISIBLE
-
-
-
+                binding.image.visibility = View.GONE
+                binding.cancel.visibility = View.GONE
+                binding.save.visibility = View.GONE
+                binding.selectToast.visibility = View.VISIBLE
+                binding.camera.visibility = View.VISIBLE
+                binding.gallery.visibility = View.VISIBLE
 
             }
             builder.setNegativeButton("NO")
-            { dialog, which -> }
+            { _, _ -> }
 
             val alertDialog = builder.create()
-
-
             alertDialog.window?.setGravity(Gravity.BOTTOM)
-
             alertDialog.show()
-
-
         }
 
-
-        activityResultLauncher  =
+        activityResultLauncher =
             registerForActivityResult(StartActivityForResult()) { result: ActivityResult ->
 
 
-                if (result.resultCode== RESULT_OK) {
+                if (result.resultCode == RESULT_OK) {
 
-                    var extras: Bundle? = result.data?.extras
+                    val extras: Bundle? = result.data?.extras
 
-                    var imageUri: Uri
+                    val imageUri: Uri
 
-                    var imageBitmap = extras?.get("data") as Bitmap
+                    val imageBitmap = extras?.get("data") as Bitmap
 
-                    var imageResult: WeakReference<Bitmap> = WeakReference(
+                    val imageResult: WeakReference<Bitmap> = WeakReference(
                         Bitmap.createScaledBitmap(
                             imageBitmap, imageBitmap.width, imageBitmap.height, false
                         ).copy(
                             Bitmap.Config.RGB_565, true
                         )
                     )
-
-                    var bm = imageResult.get()
-
+                    val bm = imageResult.get()
                     imageUri = saveImage(bm, this)
-
-
-
                     launchImageCrop(imageUri)
-
-
-
                 }
-
-                else{
-
-
-
-                }
-
-
-
             }
-
-
     }
-
-
-
-
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
@@ -196,156 +135,119 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            exTrNalStorageCode -> {
 
-        when(requestCode) {
-
-            WRITE_EXTERNAL_STORAGE_CODE -> {
-
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-
-            } else {
-                Toast.makeText(this, "Enable permissions", Toast.LENGTH_SHORT).show()
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Enable permissions", Toast.LENGTH_SHORT).show()
+                }
             }
-
-        }
-
-
-
         }
 
     }
 
-
     private fun saveEditedImage() {
-
-
         val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, finalUri)
-
         saveMediaToStorage(bitmap)
-
-
     }
 
 
     private fun saveImage(image: Bitmap?, context: Context): Uri {
 
-
-
-        var imageFolder=File(context.cacheDir,"images")
+        val imageFolder = File(context.cacheDir, "images")
         var uri: Uri? = null
 
         try {
 
             imageFolder.mkdirs()
-            var file:File= File(imageFolder,"captured_image.png")
-            var stream:FileOutputStream= FileOutputStream(file)
-            image?.compress(Bitmap.CompressFormat.JPEG,100,stream)
+            val file = File(imageFolder, "captured_image.png")
+            val stream = FileOutputStream(file)
+            image?.compress(Bitmap.CompressFormat.JPEG, 100, stream)
             stream.flush()
             stream.close()
-            uri=FileProvider.getUriForFile(context.applicationContext,"com.sunayanpradhan.imagecropper"+".provider",file)
-
+            uri = FileProvider.getUriForFile(
+                context.applicationContext,
+                "com.sunayanpradhan.imagecropper" + ".provider",
+                file
+            )
 
         } catch (e: FileNotFoundException) {
             e.printStackTrace()
-        }catch (e:IOException){
+        } catch (e: IOException) {
             e.printStackTrace()
         }
-
         return uri!!
-
     }
 
+    @SuppressLint("IntentReset")
     private fun pickFromGallery() {
 
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            intent.type = "image/*"
-            val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
-            intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
-            intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
-            startActivityForResult(intent, GALLERY_REQUEST_CODE)
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        intent.type = "image/*"
+        val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        startActivityForResult(intent, galleryCode)
     }
 
-
-    private fun pickFromCamera(){
-
-
-
+    private fun pickFromCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         activityResultLauncher.launch(intent)
 
-
-
     }
 
 
-
+    @Deprecated("Deprecated in Java")
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-
-
         when (requestCode) {
-
-            GALLERY_REQUEST_CODE -> {
+            galleryCode -> {
                 if (resultCode == Activity.RESULT_OK) {
                     data?.data?.let { uri ->
                         launchImageCrop(uri)
                     }
                 }
-
-                else{
-
-                }
             }
-
-
-
         }
 
         if (resultCode == RESULT_OK && requestCode == UCrop.REQUEST_CROP) {
-            val resultUri :Uri ?= UCrop.getOutput(data!!)
+            val resultUri: Uri? = UCrop.getOutput(data!!)
 
             setImage(resultUri!!)
 
-            finalUri=resultUri
+            finalUri = resultUri
 
-            binding.image.visibility= View.VISIBLE
-            binding.selectToast.visibility=View.GONE
-            binding.save.visibility=View.VISIBLE
-            binding.cancel.visibility=View.VISIBLE
-            binding.camera.visibility=View.GONE
-            binding.gallery.visibility=View.GONE
+            binding.image.visibility = View.VISIBLE
+            binding.selectToast.visibility = View.GONE
+            binding.save.visibility = View.VISIBLE
+            binding.cancel.visibility = View.VISIBLE
+            binding.camera.visibility = View.GONE
+            binding.gallery.visibility = View.GONE
 
         }
 
     }
 
     private fun launchImageCrop(uri: Uri) {
+        val destination: String = StringBuilder(UUID.randomUUID().toString()).toString()
+        val options: UCrop.Options = UCrop.Options()
 
-
-        var destination:String=StringBuilder(UUID.randomUUID().toString()).toString()
-        var options:UCrop.Options=UCrop.Options()
-
-
-        UCrop.of(Uri.parse(uri.toString()), Uri.fromFile(File(cacheDir,destination)))
+        UCrop.of(Uri.parse(uri.toString()), Uri.fromFile(File(cacheDir, destination)))
             .withOptions(options)
             .withAspectRatio(0F, 0F)
             .useSourceImageAspectRatio()
             .withMaxResultSize(2000, 2000)
             .start(this)
-
-
     }
 
-    private fun setImage(uri: Uri){
+    private fun setImage(uri: Uri) {
         Glide.with(this)
             .load(uri)
             .into(binding.image)
     }
-
-
-
 
     private fun checkPermission(): Boolean {
         return ContextCompat.checkSelfPermission(
@@ -398,8 +300,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
-
+    @Deprecated("Deprecated in Java")
+    override fun onBackPressed() {
+        //alert dialog
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Exit")
+        builder.setMessage("Are you sure you want to exit?")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+        builder.setPositiveButton("Yes") { _, _ ->
+            finish()
+        }
+        builder.setNegativeButton("No") { _, _ ->
+            Toast.makeText(applicationContext, "clicked No", Toast.LENGTH_LONG).show()
+        }
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show()
+    }
 
 }
